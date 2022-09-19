4893ff6caa92da92f84141aba4c5214e5c6da43d
Charles Korn <me@charleskorn.com>
2022-09-19 11:14:26 +1000

    Initial commit.

---
+++ app/src/main/kotlin/yaml/roundtrip/App.kt
@@ -0,0 +1,53 @@
+package yaml.roundtrip
+
+import org.snakeyaml.engine.v2.api.DumpSettings
+import org.snakeyaml.engine.v2.api.LoadSettings
+import org.snakeyaml.engine.v2.api.YamlOutputStreamWriter
+import org.snakeyaml.engine.v2.emitter.Emitter
+import org.snakeyaml.engine.v2.parser.ParserImpl
+import org.snakeyaml.engine.v2.scanner.StreamReader
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main() {
+    val resolvedPath = Paths.get("input.yaml").toAbsolutePath()
+    val inputText = Files.readString(resolvedPath)
+
+    val loadSettings = LoadSettings.builder()
+        .setParseComments(true)
+        .build()
+
+    val streamReader = StreamReader(loadSettings, inputText)
+    val parser = ParserImpl(loadSettings, streamReader)
+
+    val dumpSettings = DumpSettings.builder()
+        .setDumpComments(true)
+        .build()
+
+    val output = ByteArrayOutputStream()
+    val writer = object : YamlOutputStreamWriter(output, Charsets.UTF_8) {
+        override fun processIOException(e: IOException?) {
+            if (e != null) {
+                throw e
+            }
+        }
+    }
+
+    val emitter = Emitter(dumpSettings, writer)
+
+    parser.forEach {
+        emitter.emit(it)
+    }
+
+    val outputText = output.toString()
+    println("Output is:")
+    println(outputText)
+
+    if (outputText != inputText) {
+        println("Emitted YAML is different!")
+    } else {
+        println("Emitted YAML is the same.")
+    }
+}
